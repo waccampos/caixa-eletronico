a7f789685277c1c716724c091171dfc9f5115f32
waccampos <114779612+waccampos@users.noreply.github.com>
2022-10-24 14:47:38 -0300

    Create caixaeletronico.java

---
+++ caixaeletronico.java
@@ -0,0 +1,14 @@
+package caixaeletronico;
+
+public class CaixaEletronico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Caixa resultado = new Caixa();
+        resultado.Conta();
+    }
+    
+}
